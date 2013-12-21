af32a15e88274d3765b7ea3e3aa08f8464762036
Evan Lloyd <eulloyd@gmail.com>
2013-12-21 17:55:31 +0900

    Initial commit, generate  Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wikiful::Application.config.secret_key_base = '5f821f0f3b27f7e60d820692c24615cd8d4f7575915efb1d66cc6d23dc18184c0d710687132166fced895a2f385edd130aff4df39cd6d575f5a7dc84687ec3ec'
