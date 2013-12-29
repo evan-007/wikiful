76b83fa01bf687a4426519537f97f3c249f5f7b0
Evan Lloyd <eulloyd@gmail.com>
2013-12-29 09:18:54 +0900

    Changes header to show current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,10 @@ class ApplicationController < ActionController::Base
       def get_categories
       	@categories = Category.all.order(:name)
       end
+
+      def current_user
+      	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+      end
+      helper_method :current_user
+      	
 end
