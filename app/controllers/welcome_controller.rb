59b68bd3d8f9f45b399ccbff4d8bf46ecf891c22
Evan Lloyd <eulloyd@gmail.com>
2013-12-31 19:55:18 +0900

    Adds search form

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,3 +4,13 @@ class WelcomeController < ApplicationController
   	@recent_articles = Article.find(ids.sample(5))
   end
 end
+
+
+##id = Article.first.order(id: :desc).id
+##rand(0..id)
+##rand(1..id)
+#then call this 5 times 
+# or:
+# Article.find_by_sql "SELECT * ...."
+
+#or Article.all.limit(5).where("id IN (SELECT trunc(random() * 50 + 1) FROM generate_series(1,5))")
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
 	has_secure_password
 	has_many :articles
-	validates_uniqueness_of :email
+	# validates_uniqueness_of :email #not needed rails 4
 	validates :name, presence: true
-	validates :email, presence: true
+	validates :email, presence: true, uniqueness: true
 	 
 end
